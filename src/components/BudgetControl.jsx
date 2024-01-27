1476a15f119685b732ce8f4297ac64514cb5323d
Guchito <a_gaggero@hotmail.com>
2024-01-27 01:51:13 +0100

    Added available and spent

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react"
 
+const BudgetControl = ( {budget, expenses} ) => {
+    
+    const [available, setAvailable] = useState(0)
+    const [spent, setSpent] = useState(0)
+    
+    useEffect(() => {
+      const totalSpent = expenses.reduce( (total, expense) => expense.amount + total, 0 )
+      
+      const totalAvailable = budget - totalSpent
 
-const BudgetControl = (budget) => {
+      setSpent(totalSpent)
+      setAvailable(totalAvailable)
+    }, [expenses])
+    
 
     const formatAmount = (amount) => {
         return amount.toLocaleString('en-US', {
@@ -8,7 +21,7 @@ const BudgetControl = (budget) => {
             currency: 'DKK'
             }) 
     }
-
+    
   return (
     <div className="container-budget container shadow two-columns">
         <div>
@@ -16,13 +29,13 @@ const BudgetControl = (budget) => {
         </div>
         <div className="content-budget">
             <p>
-                <span>Budget: </span> {formatAmount(budget.budget)}
+                <span>Budget: </span> {formatAmount(budget)}
             </p>
             <p>
-                <span>Available: </span> {formatAmount(0)}
+                <span>Available: </span> {formatAmount(available)}
             </p>
             <p>
-                <span>Spent: </span> {formatAmount(0)}
+                <span>Spent: </span> {formatAmount(spent)}
             </p>
         </div>
     </div>
