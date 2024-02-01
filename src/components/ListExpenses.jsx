2399451a5f18c5d12ddd28f962ce39131746d5b4
Guchito <a_gaggero@hotmail.com>
2024-02-01 18:33:35 +0100

    Added filter and reset button

--- src/components/ListExpenses.jsx
+++ src/components/ListExpenses.jsx
@@ -2,21 +2,46 @@ import React from "react"
 import Expense from "./Expense"
 
 
-const ListExpenses = ({expenses, setEditExpense, deleteExpense}) => {
+const ListExpenses = ({
+      expenses, 
+      setEditExpense, 
+      deleteExpense,
+      filter,
+      filteredExpenses    
+    }) => {
     
   return (
     <div className="list-expenses container">
-        <h2>{expenses.length ? 'Expenses' : 'No expenses yet' }</h2>
+        
 
-        {expenses.map( expense => (
-            <Expense 
-                key={expense.id}
-                expense={expense}
-                setEditExpense={setEditExpense}
-                deleteExpense={deleteExpense}
-            />
-        ))}
-    
+        {filter ? (
+            <>
+              <h2>{filteredExpenses.length ? 'Expenses' : 'No expenses in this category' }</h2>
+              {filteredExpenses.map( expense => (
+                <Expense 
+                    key={expense.id}
+                    expense={expense}
+                    setEditExpense={setEditExpense}
+                    deleteExpense={deleteExpense}
+                />
+              ))}
+            </>
+          ) : (
+            <>
+            <h2>{expenses.length ? 'Expenses' : 'No expenses yet' }</h2>
+            {expenses.map( expense => (
+              <Expense 
+                  key={expense.id}
+                  expense={expense}
+                  setEditExpense={setEditExpense}
+                  deleteExpense={deleteExpense}
+              />
+            ))}
+            </>
+          )
+        }
+
+        
     </div>
   )
 }
